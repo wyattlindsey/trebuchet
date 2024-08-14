c1e27f2906d60b69ca40a3f51c6c83ef1fec78eb
Wyatt Lindsey <wyatt.b.lindsey@gmail.com>
2024-08-14 00:45:33 -0700

    initial scaffolding

---
+++ apps/electron-app/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to My 3D Game - Electron Version</h1>
+        </div>
+    );
+};
+
+export default App;
---
+++ packages/shared/inversify.config.ts
@@ -0,0 +1,9 @@
+import { Container } from 'inversify';
+// import { GameService } from './services/GameService';
+// import { SoundService } from './services/SoundService';
+
+const container = new Container();
+// container.bind(GameService).toSelf();
+// container.bind(SoundService).toSelf();
+
+export { container };
